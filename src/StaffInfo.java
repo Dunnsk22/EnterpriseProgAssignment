7c6d104adf1e5a47590812a749da5b71aa2f3805
Joshua Dunn <joshuadunn@Joshuas-MacBook-Pro.local>
2015-12-15 10:58:38 +0000

    Initial Commit

---
+++ src/StaffInfo.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+
+/**
+ * @author joshuadunn
+ *
+ */
+public class StaffInfo {
+
+	String staff_id, staff_surname, staff_forename, staff_email, staff_phone_num, address;
+
+	/**
+	 * @return the staff_id
+	 */
+	public String getStaff_id() {
+		return staff_id;
+	}
+
+	/**
+	 * @param staff_id the staff_id to set
+	 */
+	public void setStaff_id(String staff_id) {
+		this.staff_id = staff_id;
+	}
+
+	/**
+	 * @return the staff_surname
+	 */
+	public String getStaff_surname() {
+		return staff_surname;
+	}
+
+	/**
+	 * @param staff_surname the staff_surname to set
+	 */
+	public void setStaff_surname(String staff_surname) {
+		this.staff_surname = staff_surname;
+	}
+
+	/**
+	 * @return the staff_forename
+	 */
+	public String getStaff_forename() {
+		return staff_forename;
+	}
+
+	/**
+	 * @param staff_forename the staff_forename to set
+	 */
+	public void setStaff_forename(String staff_forename) {
+		this.staff_forename = staff_forename;
+	}
+
+	/**
+	 * @return the staff_email
+	 */
+	public String getStaff_email() {
+		return staff_email;
+	}
+
+	/**
+	 * @param staff_email the staff_email to set
+	 */
+	public void setStaff_email(String staff_email) {
+		this.staff_email = staff_email;
+	}
+
+	/**
+	 * @return the staff_phone_num
+	 */
+	public String getStaff_phone_num() {
+		return staff_phone_num;
+	}
+
+	/**
+	 * @param staff_phone_num the staff_phone_num to set
+	 */
+	public void setStaff_phone_num(String staff_phone_num) {
+		this.staff_phone_num = staff_phone_num;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
